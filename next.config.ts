9ec7fba1dc74a3a638af38584101dbcd21a724f3
Aman Sharma <sharmaa1518@gmail.com>
2025-10-30 15:05:08 +0530

    fix : image tags and links for DB and next config file

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "2mb",
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co", // for images
+      },
+    ],
+  },
 };
 
 export default nextConfig;
